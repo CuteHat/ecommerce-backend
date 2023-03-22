9531fc7a3f8fbe4227fb3a1fbaa4093b7a0893f2
jimmy <jimmy@ownic.com>
2023-03-22 22:00:40 +0400

    init other APIs as well

---
+++ pad/src/test/java/com/example/pad/PadApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.pad;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PadApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
